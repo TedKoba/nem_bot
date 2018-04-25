354410e788cff3f14efc882becc2f67d54a7035c
Takuma Kobayashi <taq.kob@gmail.com>
2018-04-25 15:54:24 +0900

    deleted comments

--- nem_bot/celery.py
+++ nem_bot/celery.py
@@ -3,7 +3,6 @@ import os
 from celery import Celery
 from django.conf import settings
 
-# Indicate Celery to use the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nem_bot.settings')
 
 app = Celery('nem_bot',
@@ -11,5 +10,4 @@ app = Celery('nem_bot',
 )
 app.config_from_object('django.conf:settings')
 
-# This line will tell Celery to autodiscover all your tasks.py that are in your app folders
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
